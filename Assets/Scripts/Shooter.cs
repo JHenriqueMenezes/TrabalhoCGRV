136167c4302d592a3657441e2b589b8486041e90
João Henrique Menezes de Souza <117228319+JHenriqueMenezes@users.noreply.github.com>
2025-11-24 19:41:48 -0300

    Add AI firing behavior and object pooling support

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,14 +8,27 @@ public class Shooter : MonoBehaviour
     [SerializeField] float projectileLifetime = 5f;
     [SerializeField] float fireRate = 0.2f;
     [SerializeField] float projectileSpawnOffset = 0.5f;
+    [SerializeField] bool isIa = false;
 
     public bool isFiring;
     Coroutine fireCoroutine;
     AudioManager audioManager;
 
-    void Update()
+    ObjectPool objectPool;
+
+    void Awake()
     {
         audioManager = FindFirstObjectByType<AudioManager>();
+        objectPool = GetComponent<ObjectPool>();
+    }
+
+    void Start()
+    {
+        isFiring = isIa;
+    }
+
+    void Update()
+    {
         Fire();
     }
 
@@ -37,13 +50,23 @@ public class Shooter : MonoBehaviour
     {
         while (true)
         {
+            GameObject projectile;
             Vector3 spawnPosition = transform.position + transform.up * projectileSpawnOffset;
-            GameObject projectile = Instantiate(projectilePrefab, spawnPosition, transform.rotation);
+
+            if (objectPool != null)
+            {
+                projectile = objectPool.Get();
+                projectile.transform.position = spawnPosition;
+                projectile.transform.rotation = transform.rotation;
+            }
+            else
+            {
+                projectile = Instantiate(projectilePrefab, spawnPosition, transform.rotation);
+                Destroy(projectile, projectileLifetime);
+            }
 
             Rigidbody2D projectileRB = projectile.GetComponent<Rigidbody2D>();
             projectileRB.linearVelocity = (Vector2)(transform.up * projectileSpeed);
-
-            Destroy(projectile, projectileLifetime);
 
             audioManager.PlayShootingSFX();
             
@@ -51,4 +74,3 @@ public class Shooter : MonoBehaviour
         }
     }
 }
-
